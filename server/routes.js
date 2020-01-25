fbf39474a5d3881dff23713484f153204bf41bd4
arianna choza <unachoza@gmail.com>
2020-01-25 01:47:58 -0500

    reconfig to spot serve

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ const cancelController = require('./controller.js')
 console.log("this is cancelController", cancelController)
 
 router.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://spot-server.herokuapp.com/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
     
