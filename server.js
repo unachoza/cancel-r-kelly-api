e8ae90671cd988a0c8d1d510cd0243a8c7b9ff25
arianna choza <unachoza@gmail.com>
2020-01-26 18:18:32 -0500

    adding cors allow

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const db = require('./db/config')
-
+const getDb = require('../db/config');
+const db = getDb()
+const cors = require('cors')
 const server = express();
 
 server.use(logger('dev'));
@@ -19,6 +20,27 @@ server.get('/', (req, res) => {
 const cancelRoutes = require('./server/routes');
 server.use('/db', cancelRoutes);
 
+
+server.use(cors({ origin: 'http://spotify-cancel.herokuapp.com/', credentials: true }))
+server.use( (req, res, next)=> {
+    /*var err = new Error('Not Found');
+     err.status = 404;
+     next(err);*/
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://spot-server.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+  //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Pass to next layer of middleware
+    next();
+  });
 server.use('*', (req, res) => {
   res.status(400).json({
     message: 'Endpoint not found. But you can definitely find it; Keep trying!',
@@ -28,24 +50,3 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-// app.use(cors({ origin: 'http://localhost3001/db:3030', credentials: true }))
-// server.use( (req, res, next)=> {
-//     /*var err = new Error('Not Found');
-//      err.status = 404;
-//      next(err);*/
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'localhost:3001/db');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-
-//   //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-//     // Pass to next layer of middleware
-//     next();
-//   });
