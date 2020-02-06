00172b21d84a3c3557c6658e371f5c86e4a111f9
arianna choza <unachoza@gmail.com>
2020-02-06 12:31:08 -0500

    copies smartbrainapi

--- server.js
+++ server.js
@@ -5,43 +5,43 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 // const proxy = require('http-proxy-middleware'); 
 const server = express();
-
+server.use(cors())
 server.use(logger('dev'));
 server.use(express.static('public'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
-var corsOptions = {
-  origin: 'http://spotify-cancel.herokuapp.com/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//   origin: 'http://spotify-cancel.herokuapp.com/',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 server.get('/', (req, res) => {
   res.send('hello world; you have connected');
 });
 
-server.get('/users/unique', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
-})
+// server.get('/users/unique', cors(corsOptions), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// })
  
 
 
-server.use(cors({ origin: 'http://spotify-cancel.herokuapp.com/', credentials: true }))
-server.use( (req, res, next)=> {
+// server.use(cors({ origin: 'http://spotify-cancel.herokuapp.com/', credentials: true }))
+// server.use( (req, res, next)=> {
    
-    res.setHeader('Access-Control-Allow-Origin', 'http://spotify-cancel.herokuapp.com/');
+//     res.setHeader('Access-Control-Allow-Origin', 'https://spot-server.herokuapp.com/');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
 
-  //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
   
 const cancelRoutes = require('./server/routes');
 server.use('/db', cancelRoutes);
